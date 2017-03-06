3286e5d4ae7943f78306fe778532da7c98fd9383
Chun-Yi Wang <chunyi_wang@foxlink.com>
2017-03-06 09:40:46 +0800

    1.加入Model:ChargeItem 2.增加Partial View:Charge.cshtml，用於顯示記帳的結果

--- LedgerMVC/Controllers/ChargeController.cs
+++ LedgerMVC/Controllers/ChargeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LedgerMVC.Models;
 
 namespace LedgerMVC.Controllers
 {
     public class ChargeController : Controller
     {
         // GET: Charge
-        public ActionResult Index()
+        public ActionResult ChargeMoney()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChargeMoney(ChargeItem chargeItem)
         {
             return View();
         }
---
+++ LedgerMVC/Models/ChargeItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LedgerMVC.Models
+{
+    public class ChargeItem
+    {
+        public string ChargeType { get; set; }
+        public int ChargePrice { get; set; }
+        public string Memo { get; set; }
+
+        public DateTime ChargeDate { get; set; }
+
+        public int ChargeRecordId { get; set; }
+    }
+}
