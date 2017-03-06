b91b8649258b7ca74b4cfc7d58aaa3d225f2351c
Chun-Yi Wang <chunyi_wang@foxlink.com>
2017-03-06 17:00:00 +0800

    1.加入ChargeController，並增加三個Action (ChargeMoney 顯示記帳紀錄、Index輸出首頁、ChargeMoney(ChargeItem)用於新增計帳項目) 2.此版本先將ChargeMony用ActionChild、新增計帳項目的畫面使用Pratial View (_AddCharge)

--- LedgerMVC/Controllers/ChargeController.cs
+++ LedgerMVC/Controllers/ChargeController.cs
@@ -9,16 +9,64 @@ namespace LedgerMVC.Controllers
 {
     public class ChargeController : Controller
     {
-        // GET: Charge
-        public ActionResult ChargeMoney()
+        public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult ChargeMoney()
+        {
+            var ChargeList = this.GetChargeItems();   
+            return View(ChargeList);
         }
 
         [HttpPost]
         public ActionResult ChargeMoney(ChargeItem chargeItem)
         {
             return View();
+        }
+
+
+        private IEnumerable<ChargeItem> GetChargeItems()
+        {
+
+            DateTime dt=DateTime.Now;
+            var ChargeItem1 = new ChargeItem
+            {
+                ChargeRecordId = 1,
+                ChargeDate = dt,
+                ChargeType = "支出",
+                ChargePrice = 1000,
+                Memo = ""
+            };
+
+            var ChargetItem2 = new ChargeItem
+            {
+                ChargeRecordId = 2,
+                ChargeDate = dt.AddDays(-1),
+                ChargeType = "收入",
+                ChargePrice = 40000,
+                Memo = "打工收入"
+            };
+
+            var ChargetItem3 = new ChargeItem
+            {
+                ChargeRecordId = 3,
+                ChargeDate = dt.AddDays(-10),
+                ChargeType = "收入",
+                ChargePrice = 100000,
+                Memo = "打工收入"
+            };
+
+            List<ChargeItem> ChargeList = new List<ChargeItem>
+            {
+                ChargeItem1,
+                ChargetItem2,
+                ChargetItem3
+            };
+
+            return ChargeList;
         }
     }
 }
