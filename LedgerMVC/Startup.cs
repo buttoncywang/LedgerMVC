a6ea03c1a0f35f2c2dd0253b9cfee4f2782ff8b3
Chun-Yi Wang <chunyi_wang@foxlink.com>
2017-03-06 08:50:50 +0800

    加入專案檔案。

---
+++ LedgerMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LedgerMVC.Startup))]
+namespace LedgerMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
