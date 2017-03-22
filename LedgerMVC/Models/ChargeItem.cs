0a8f0b5238801cf8c9408ee9187f424c831bfba4
buttoncywang <cywang0404@g2.nctu.edu.tw>
2017-03-22 23:31:46 +0800

    1.加入jQuery UI，用於顯示日期挑選 2.在 Models/ChargeItem.cs 加入驗證條件

--- LedgerMVC/Models/ChargeItem.cs
+++ LedgerMVC/Models/ChargeItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,19 @@ namespace LedgerMVC.Models
 {
     public class ChargeItem
     {
+        [Display(Name = "類型")]
+        [Required(ErrorMessage = "必需選擇記帳類型")]
         public string ChargeType { get; set; }
+        [Display(Name = "金額")]
+        [Required(ErrorMessage = "金額必需大於0元")]
+        [Range(1, int.MaxValue, ErrorMessage = "金額必需大於0元")]
         public int ChargePrice { get; set; }
+        [Required(ErrorMessage = "必需填寫備註")]
+        [Display(Name = "備註")]
+        [StringLength(100, MinimumLength = 1)]
         public string Memo { get; set; }
-
+        [Display(Name = "日期")]
+        [Required(ErrorMessage = "請選擇日期")]
         public DateTime ChargeDate { get; set; }
 
         public int ChargeRecordId { get; set; }
